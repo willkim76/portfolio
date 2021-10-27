3812d8e08ae389242470fa286f5c6f7b86c3909c
William Kim <williamkim76@outlook.com>
2021-10-27 11:30:40 -0700

    Initial VersionControl commit.

---
+++ VersionControl/Alpha.java
@@ -0,0 +1,13 @@
+/*
+ * This program is to demonstrate basic Version Control using Git/GitHub
+ *
+ */
+package PackageAlpha;
+
+public class Alpha {
+	private static float version = 0.00f;
+
+	public static void main(String[] args) {
+		System.out.println(version);
+	}
+}
---
+++ VersionControl/Bravo.java
@@ -0,0 +1,13 @@
+/*
+ * Bravo is a program that demonstrates fundemental version control
+ */
+
+package PackgeAlpha;
+
+public class Bravo {
+	private static final String version = "v0.0.0";
+
+	public static void main(String[] args) {
+		System.out.println(version);
+	}
+}
